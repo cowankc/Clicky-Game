f9143589fc08b94e8a7353dce48c0455c1955574
cowankc <cowankc92@live.com>
2019-12-23 19:02:09 -0600

    added component files and worked on render funtion

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
+import characters from './characters.json'
+import Character from './components/Character'
+import Score from './components/Score'
+import Container from './components/Container'
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+  state = {
+    characters, 
+    score: 0,
+    topscore: 0
+  };
+
+  render() {
+    return (
+      <Container>
+        <Score score={this.state.score} topscore={this.state.topscore}>South Park Click Game</Score>
+        {this.state.characters.map(character => (
+          <Character
+            clickCount={this.clickCount}
+            id={character.id}
+            key={character.id}
+            image={character.image}
+            />
+        ))}
+      </Container>
+    )
+  }
 }
 
 export default App;
