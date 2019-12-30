3c007989841a070f355c2d230ff3b700c649492f
cowankc <cowankc92@live.com>
2019-12-30 12:19:43 -0600

    Updates

---
+++ precache-manifest.f0f893584d263135de5762908f2a041d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc2b123e0770e9ae031d92ac39f35cb",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9f892611343be2dbe347",
+    "url": "/Clicky-Game/static/css/main.c2690d18.chunk.css"
+  },
+  {
+    "revision": "6fd066877a661375e886",
+    "url": "/Clicky-Game/static/js/2.61ba17e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.61ba17e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f892611343be2dbe347",
+    "url": "/Clicky-Game/static/js/main.82a9a61f.chunk.js"
+  },
+  {
+    "revision": "454fdfe5bfc5980f98d3",
+    "url": "/Clicky-Game/static/js/runtime-main.4d5d914a.js"
+  }
+]);
